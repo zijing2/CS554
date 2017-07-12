8825f7f51c69ee8a11ae6f0786700a4314edd928
黄梓敬 <415940392@qq.com>
2017-07-12 17:31:17 -0400

    lab5

---
+++ lab5/routes/api.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const data = require("../data");
+const db = data.db;
+
+const bluebird = require("bluebird");
+const flat = require("flat");
+const unflatten = flat.unflatten
+const redis = require('redis');
+const client = redis.createClient();
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+router.get("/api/people/history",async (req, res) => {
+         var len;
+         var people_arr = [];
+        if(await client.llenAsync('list')<19){
+            len = await client.llenAsync('list');
+            client.llen
+        }else{
+            len = 19;
+        }
+        people_arr = await client.lrangeAsync('list',0,len);
+        for(var i=0;i<people_arr.length;i++){
+            people_arr[i] = JSON.parse(people_arr[i]);
+        }
+        res.status(200).json(people_arr);
+});
+
+router.get("/api/people/:id",async (req, res) => {
+        //check redis data
+         let doesIdExist = await client.existsAsync(req.params.id);
+         if(doesIdExist){
+            let people = await client.getAsync(req.params.id);
+            await client.lpush('list',people);
+            res.status(200).json(JSON.parse(people));
+         }else{
+             try {
+                 var people = await db.getById(req.params.id);
+                 let setResult = await client.setAsync(req.params.id, JSON.stringify(people));
+                 await client.lpush('list',JSON.stringify(people));
+                 res.status(200).json(people);
+             } catch (error) {
+                 res.status(404).json({error: "person not found"});
+             }
+         }  
+});
+
+
+
+module.exports = router;
